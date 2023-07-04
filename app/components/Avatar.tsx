7cfda695aba6db6f03cd955ee88f6b938ca606be
Julia Sankevich <78703258+snkvch@users.noreply.github.com>
2023-07-04 15:36:19 +0300

    feat: add navbarUI

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      src="/images/placeholder.jpeg"
+      alt="Avatar"
+      height="30"
+      width="30"
+    />
+  );
+};
+
+export default Avatar;
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+import Navbar from "./components/navbar/Navbar";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
