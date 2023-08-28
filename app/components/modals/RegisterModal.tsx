b473f81195966eff184678e48f2180342bfe0923
Julia Sankevich <78703258+snkvch@users.noreply.github.com>
2023-08-28 13:45:32 -0400

    feat: add Trips, Reservations and Favorites pages

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -40,6 +40,7 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        loginModal.onOpen();
         registerModal.onClose();
       })
       .catch((error) => {
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -10,12 +10,14 @@ import useRentModal from "@/app/hooks/useRentModal";
 
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
+import { useRouter } from "next/navigation";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -71,9 +73,9 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My trips" />
-                <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
+                <MenuItem onClick={() => router.push("/trips")} label="My trips" />
+                <MenuItem onClick={() => router.push("/favorites")} label="My favorites" />
+                <MenuItem onClick={() => router.push("/reservations")} label="My reservations" />
                 <MenuItem onClick={() => {}} label="My properties" />
                 <MenuItem onClick={() => {}} label="Airbnb my home" />
                 <hr />
